1f072e2582cdf69de4b930cfd72e6df03fe0d126
Blake Watters <blake@layer.com>
2014-07-21 21:23:04 -0700

    Alias the namesake dummy class to avoid conflicts when using the generated podspec

--- lib/mangle.rb
+++ lib/mangle.rb
@@ -12,15 +12,20 @@ module Symbols
       |file| file !~ /#{pod_name}/
     end
 
-    all_syms = []
+    dummy_alias = alias_symbol "PodsDummy_Pods_#{pod_name}", pod_name
+    all_syms = [dummy_alias]
 
     pod_libs.each do |pod_lib|
       syms = Symbols.symbols_from_library(pod_lib)
-      all_syms += syms.map! { |sym| sym + "=Pod#{pod_name}_" + sym }
+      all_syms += syms.map! { |sym| alias_symbol sym, pod_name }
     end
 
     "GCC_PREPROCESSOR_DEFINITIONS='${inherited} #{all_syms.uniq.join(' ')}'"
   end
 
-  module_function :mangle_for_pod_dependencies
+  def alias_symbol(sym, pod_name)
+    sym + "=Pod#{pod_name}_" + sym
+  end
+
+  module_function :mangle_for_pod_dependencies, :alias_symbol
 end
