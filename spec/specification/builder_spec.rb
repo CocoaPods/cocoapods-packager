0c97bc62674359c28da6f66e9b0eb12eae448d7c
Michael Melanson <michael.melanson@toushay.com>
2015-04-09 11:46:10 -0400

    Add tests.

--- spec/specification/builder_spec.rb
+++ spec/specification/builder_spec.rb
@@ -2,22 +2,38 @@ require File.expand_path('../../spec_helper', __FILE__)
 
 module Pod
   describe Builder do
-    before do
-      @spec = Specification.from_file('spec/fixtures/Builder.podspec')
-
-      @builder = Builder.new(nil, nil, nil, @spec, nil, nil)
-    end
-
     describe 'Xcodebuild command' do
+      describe 'compiler flags' do
+        before do
+          @spec = Specification.from_file('spec/fixtures/Builder.podspec')
+          @builder = Builder.new(nil, nil, nil, @spec, nil, nil)
+        end
 
-      it "includes proper compiler flags for iOS" do
-        @builder.expects(:xcodebuild).with('GCC_PREPROCESSOR_DEFINITIONS=\'PodsDummy_Pods_Builder=PodsDummy_PodPackage_Builder\' -DBASE_FLAG -DIOS_FLAG ARCHS="x86_64 i386 arm64 armv7 armv7s"').returns(nil)
-        @builder.compile(Platform.new(:ios))
+        it "includes proper compiler flags for iOS" do
+          @builder.expects(:xcodebuild).with('GCC_PREPROCESSOR_DEFINITIONS=\'PodsDummy_Pods_Builder=PodsDummy_PodPackage_Builder\' -DBASE_FLAG -DIOS_FLAG ARCHS="x86_64 i386 arm64 armv7 armv7s"').returns(nil)
+          @builder.compile(Platform.new(:ios))
+        end
+
+        it "includes proper compiler flags for OSX" do
+          @builder.expects(:xcodebuild).with("GCC_PREPROCESSOR_DEFINITIONS='PodsDummy_Pods_Builder=PodsDummy_PodPackage_Builder' -DBASE_FLAG -DOSX_FLAG").returns(nil)
+          @builder.compile(Platform.new(:osx))
+        end
       end
 
-      it "includes proper compiler flags for OSX" do
-        @builder.expects(:xcodebuild).with("GCC_PREPROCESSOR_DEFINITIONS='PodsDummy_Pods_Builder=PodsDummy_PodPackage_Builder' -DBASE_FLAG -DOSX_FLAG").returns(nil)
-        @builder.compile(Platform.new(:osx))
+      describe 'on build failure' do
+        before do
+          @spec = Specification.from_file('spec/fixtures/Builder.podspec')
+          @builder = Builder.new(nil, nil, nil, @spec, nil, nil)
+        end
+
+        it 'dumps report and terminates' do
+          UI::BuildFailedReport.expects(:report).returns(nil)
+
+          should.raise SystemExit do
+            # TODO: check that it dumps report
+            @builder.compile(Platform.new(:ios))
+          end
+        end
       end
     end
   end
