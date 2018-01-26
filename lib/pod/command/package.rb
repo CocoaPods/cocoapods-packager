c45ba258c261c2263b239e2ac42526a466eaab8e
刘思兰 <liusilan@douyu.tv>
2018-01-26 20:05:14 +0800

    还原

--- lib/pod/command/package.rb
+++ lib/pod/command/package.rb
@@ -1,7 +1,7 @@
 require 'tmpdir'
 module Pod
   class Command
-    class MyPackage < Command
+    class Package < Command
       self.summary = 'Package a podspec into a static library.'
       self.arguments = [
         CLAide::Argument.new('NAME', true),
