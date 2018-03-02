23cec11b987dff41e435b5b8ecf5ca5d450d62af
Muhammed Yavuz Nuzumlalı <manuyavuz@gmail.com>
2018-03-02 15:39:09 +0300

    Update specs

--- spec/specification/builder_spec.rb
+++ spec/specification/builder_spec.rb
@@ -6,24 +6,26 @@ module Pod
       describe 'compiler flags' do
         before do
           @spec = Specification.from_file('spec/fixtures/Builder.podspec')
-          @builder = Builder.new(nil, nil, nil, nil, nil, nil, @spec, nil, nil, nil, nil, nil, nil)
         end
 
         it "includes proper compiler flags for iOS" do
+          @builder = Builder.new(Platform.new(:ios), [], nil, nil, nil, nil, @spec, nil, nil, nil, nil, nil, nil)
+          puts @builder.inspect
           @builder.expects(:xcodebuild).with("GCC_PREPROCESSOR_DEFINITIONS='$(inherited) PodsDummy_Pods_Builder=PodsDummy_PodPackage_Builder' -DBASE_FLAG -DIOS_FLAG", "ARCHS='x86_64 i386 arm64 armv7 armv7s' OTHER_CFLAGS='-fembed-bitcode -Qunused-arguments'").returns(nil)
-          @builder.send(:compile, Platform.new(:ios))
+          @builder.send(:compile)
         end
 
         it "includes proper compiler flags for OSX" do
+          @builder = Builder.new(Platform.new(:osx), [], nil, nil, nil, nil, @spec, nil, nil, nil, nil, nil, nil)
           @builder.expects(:xcodebuild).with("GCC_PREPROCESSOR_DEFINITIONS='$(inherited) PodsDummy_Pods_Builder=PodsDummy_PodPackage_Builder' -DBASE_FLAG -DOSX_FLAG", nil).returns(nil)
-          @builder.send(:compile, Platform.new(:osx))
+          @builder.send(:compile)
         end
       end
 
       describe 'on build failure' do
         before do
           @spec = Specification.from_file('spec/fixtures/Builder.podspec')
-          @builder = Builder.new(nil, nil, nil, nil, nil, nil, @spec, nil, nil, nil, nil)
+          @builder = Builder.new(Platform.new(:ios), [], nil, nil, nil, nil, @spec, nil, nil, nil, nil, nil, nil)
         end
 
         it 'dumps report and terminates' do
@@ -31,7 +33,7 @@ module Pod
 
           should.raise SystemExit do
             # TODO: check that it dumps report
-            @builder.send(:compile, Platform.new(:ios))
+            @builder.send(:compile)
           end
         end
       end
